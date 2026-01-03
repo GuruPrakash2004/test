efdbff9f7157814f0498d1c8228025c1eabfe82f
Guru prakash <guruprakash832636@gmail.com>
2026-01-03 10:16:51 +0530

    formated version

--- test1/src/App.jsx
+++ test1/src/App.jsx
@@ -1,25 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import ContainerList from './components/ContainerList'
-import CounterAll from './components/CounterAll'
+import { useState } from "react";
+import "./App.css";
+import ContainerList from "./components/ContainerList";
+import CounterAll from "./components/CounterAll";
 
 function App() {
-
-  const [value,setValue] = useState(0);
-  
+  const [value, setValue] = useState(0);
 
   return (
     <>
       <div>
-          <h3 className='text-center'>count:{value}</h3>
-          <div className='d-flex p-4'>
-          <ContainerList setValue={setValue}/>
+        <h3 className="text-center">count:{value}</h3>
+        <div className="d-flex p-4">
+          <ContainerList setValue={setValue} />
+        </div>
       </div>
-      </div>
-
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- test1/src/components/CounterAll.jsx
+++ test1/src/components/CounterAll.jsx
@@ -2,19 +2,15 @@ import { useState } from "react";
 import Container from "./Container";
 import Button from "./Button";
 
-function CounterAll(){
- 
-    
-    const [value,setValue] = useState(0);
+function CounterAll() {
+  const [value, setValue] = useState(0);
 
-    return(
+  return (
     <>
-        <h2>Count:{value}</h2> 
-        <Button setValue={setValue}/>
-       
+      <h2>Count:{value}</h2>
+      <Button setValue={setValue} />
     </>
-    )
+  );
 }
-
 
 export default CounterAll;
